2adf00c9e10189c3f2d91543d0990de97dc93841
Sambo Chea <sombochea100@gmail.com>
2018-09-17 18:57:43 +0700

    add some assets and rename projects

---
+++ WebInfrastrutures/Sartup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WebInfrastrutures
+{
+    public class Sartup
+    {
+        public static void Main()
+        {
+            int x = 0;
+
+            if (x >= 1)
+                Environment.Exit(0);
+
+            Console.WriteLine("Web Infrastruture is running...");
+            Console.ReadKey();
+        }
+    }
+}
