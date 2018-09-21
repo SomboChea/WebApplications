fa207478362ef83056d4924bf0bb7064228d1092
Sambo Chea <sombochea100@gmail.com>
2018-09-21 17:56:12 +0700

    nearly completely on backend view

--- WebFrontEnd/Program.cs
+++ WebFrontEnd/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WebFrontEnd
+namespace SE.WebFrontEnd
 {
     public class Program
     {
